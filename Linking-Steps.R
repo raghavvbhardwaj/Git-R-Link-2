8cd64e590c55e4125be2d6f295ea6412011804c5
Raghav Bhardwaj <bhardwajraghav97@gmail.com>
2020-07-22 11:54:23 -0700

    Steps followed to make changes to online Git repository via local RStudio edits

---
+++ Linking-Steps.R
@@ -0,0 +1,4 @@
+print("1. First a Repository was created in Github")
+print("2. A project was created through version control in R with the ^ Git Repository")
+print("3. A new r script file was created with this code and saved in the default R proj folder")
+print("4. The file was then staged and commited followed by a push on RStudio")
